d2fd04e795aeda986d2490d2b9ac8fcbf719184e
Julian Lanzl <julian.lanzl@fau.de>
2024-11-04 11:04:16 +0100

    Made the package installable

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="math_quiz",
+    version="1.0.0",
+    description="A fun math quiz generator",
+    license="Apache License 2.0",
+    packages=find_packages()
+)
